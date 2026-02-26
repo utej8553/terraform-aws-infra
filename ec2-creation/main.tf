e58b010bf4a8ddb4f16ff8230fb2617d15b712df
Ubuntu <ubuntu@ip-172-31-13-226.ec2.internal>
2026-02-26 14:20:43 +0000

    added

--- ec2-creation/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "demo" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "terraform-ec2-demo"
-  }
-}
--- ec2-creation/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "public_ip" {
-  value = aws_instance.demo.public_ip
-}
