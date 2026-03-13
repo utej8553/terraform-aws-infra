f2e7424e825521beda20a9112348e462d51f64d1
Ubuntu <ubuntu@ip-172-31-35-191.ec2.internal>
2026-03-13 14:43:59 +0000

    tf-5(2)

--- terraform-5/main.tf
+++ terraform-5/main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "terraform5"{
 	ami = "ami-02dfbd4ff395f2a1b"
 	instance_type = var.instance_type
+	count = length(var.ami_names)
 	tags = {
-		count = length(var.ami_names)
 		Name = var.ami_names[count.index]
 	}
 
