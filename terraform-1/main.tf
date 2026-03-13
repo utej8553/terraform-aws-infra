bfbcb78a8f233f1f41d30f8c4abb820dce49988d
Ubuntu <utej21631@gmail.com>
2026-03-13 15:05:53 +0000

    test commit

--- terraform-1/main.tf
+++ terraform-1/main.tf
@@ -1,7 +1,7 @@
-resource "aws_instance" "ec2_demo"{
-	ami="ami-0b6c6ebed2801a5cb"
-	instance_type="t3.micro"
-	tags={
-		Name = "TerraformEC2"
+resource "aws_instance" "ec2-terraform"{
+	ami = "ami-0b6c6ebed2801a5cb"
+	instance_type = "t3.micro"
+	tags = {
+		Name = "Terraform-EC2"
 	}
 }
